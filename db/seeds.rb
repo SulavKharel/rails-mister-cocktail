7d7869a54d999a3eb66f014329246581eba7e19a
sulav kharel <kharel.sulav2016@gmail.com>
2019-05-24 12:12:18 +0900

    created a root path

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@ Ingredient.destroy_all
 url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 drink_serialized = open(url).read
 data = JSON.parse(drink_serialized)
-p data['drinks'].first
 data["drinks"].each do |liquor|
   Ingredient.create!(name: liquor["strIngredient1"])
 end
